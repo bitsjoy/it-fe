cb71bdac609829e16d8e298301d693b7576b0d68
Neelesh <nsharma1@me.iitr.ac.in>
2022-07-28 02:11:06 +0530

    A

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ Routes,
 Route
 } from 'react-router-dom';
 
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import MainForm from './pages/home';
@@ -15,23 +15,18 @@ import Profile from './pages/profilePage';
 import NoteMaker from './pages/noteMaker';
 import VerifyOneTime from './pages/verifyOneTimePayment';
 import { useEffect, useState } from 'react';
-import { AndroidOutlined, AppleOutlined, WindowsOutlined } from '@ant-design/icons';
+import { AndroidOutlined, AppleOutlined, SyncOutlined, WindowsOutlined } from '@ant-design/icons';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import { Button, Input, Modal } from 'antd'; 
+import { btnBackgroundColor } from './uiConfig';
 
 let deferredPrompt;  
     
 function App() {
   const [installable, setInstallable] = useState(false);
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      if(installable == true){
-        document.getElementById('installBtn').style.transform = 'translate(-500px, 0px)';
-      }
-    }, 500);
+  const [installAppModal, setInstallAppModal] = useState(true);
     
-  }, [installable])
-
+ 
   useEffect(() => {
     window.addEventListener("beforeinstallprompt", (e) => {
       // Prevent the mini-infobar from appearing on mobile
@@ -40,6 +35,7 @@ function App() {
       deferredPrompt = e;
       // Update UI notify the user they can install the PWA
       setInstallable(true);
+      setInstallAppModal(true);
     });
 
     window.addEventListener('appinstalled', () => {
@@ -69,12 +65,7 @@ function App() {
  {/* <button onClick={()=>{
   setInstallable(!installable);
  }} />  */}
-{
-installable && 
-          <button id="installBtn" style={{ position: 'fixed', bottom: '0px', right: '-500px', zIndex: '999', fontSize: '17px', fontWeight: '500', padding: '10px 30px', border: '1px solid  #009e60', backgroundColor: '#009e60', color: 'white'}} onClick={handleInstallClick}>
-            Install app &nbsp; <AndroidOutlined /> <AppleOutlined /> <WindowsOutlined /> 
-          </button> 
-        }
+ 
       
       <Router>
       <ToastContainer
@@ -97,6 +88,30 @@ installable &&
 
         </Routes> 
       </Router>
+
+      <Modal title="Install Bitsjoy App" visible={installable && installAppModal} onOk={()=>{}} onCancel={()=>{setInstallAppModal(false)}}
+
+footer={[
+  <Button key="back" type="link" onClick={() => {setInstallAppModal(false)}}>
+    Remind me later
+  </Button>
+   
+]}>
+<div align="center">
+ Visit <span style={{color: btnBackgroundColor}}>bitsjoy.com</span> via chrome on any device (smartphone / PC / laptop) to install the app on that device
+  <br/>
+  <br/>  
+<Button size='large' id="installBtn" style={{ fontWeight: '500', border: '1px solid  #009e60', backgroundColor: '#009e60', color: 'white'}} onClick={handleInstallClick}>
+            Install on this device
+          </Button> 
+          <br/>
+          <br/>
+   <span style={{fontSize: '20px', color: 'silver'}}> <AndroidOutlined /> <AppleOutlined /> <WindowsOutlined /> </span>
+
+          </div>
+          <br/> 
+      </Modal>
+
     </div>
   );
 }
